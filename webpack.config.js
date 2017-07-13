13b05c61694595c4795883dff48655b237a74e35
frank <vnussolution@gmail.com>
2017-07-12 17:53:13 -0700

    finished all sections, to continue with media queries

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                 use: ['html-loader']
             },
             {
-                test: /\.(jpg|png)$/,
+                test: /\.(jpg|png|svg)$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
